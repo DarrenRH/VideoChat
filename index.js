0a4aa7b1dec44fa2d3ce4d809b65f7ad1e1d89e4
DarrenRH <darefiveare@gmail.com>
2021-01-10 17:42:09 +0000

    adds event broadcasting for signalling

--- index.js
+++ index.js
@@ -18,14 +18,34 @@ io.on("connection",function(socket){
         var room = rooms.get(roomName);
         if(room == undefined){
             socket.join(roomName);
-            console.log("Room Created");
+            socket.emit("created");
         }
         else if(room.size == 1){
             socket.join(roomName);
-            console.log("Room joined");
+            socket.emit("joined");
         }
         else{
-            console.log("Room full");
+            socket.emit("full");
         }
+    });
+
+    socket.on("ready",function(roomName){
+        console.log("Ready");
+        socket.broadcast.to(roomName).emit("ready");
+    });
+
+    socket.on("candidate",function(candidate, roomName){
+        console.log("Candidate");
+        socket.broadcast.to(roomName).emit("candidate", candidate);
+    });
+
+    socket.on("offer",function(offer, roomName){
+        console.log("Offer");
+        socket.broadcast.to(roomName).emit("offer", offer);
+    });
+
+    socket.on("answer",function(answer, roomName){
+        console.log("Answer");
+        socket.broadcast.to(roomName).emit("answer", answer);
     });
 });
